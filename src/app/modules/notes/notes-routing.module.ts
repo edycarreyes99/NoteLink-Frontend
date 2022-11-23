fae6c03f7cb819ea52c1b85d3a6701c391f11753
Edycar Reyes <edycarreyes@gmail.com>
2022-11-22 19:37:09 -0600

    Generated the routes for the 'notes' module

--- src/app/modules/notes/notes-routing.module.ts
+++ src/app/modules/notes/notes-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NotesViewComponent} from "./components/notes-view/notes-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NotesViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/notes/notes.module.ts
+++ src/app/modules/notes/notes.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotesRoutingModule } from './notes-routing.module';
+import { NotesViewComponent } from './components/notes-view/notes-view.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotesViewComponent
+  ],
   imports: [
     CommonModule,
     NotesRoutingModule
