9ae4af29f94cfdd655efa3442023a33ff7de6a28
Edycar Reyes <edycarreyes@gmail.com>
2022-11-24 02:21:45 -0600

    tried to design in the air the note-list-item component

--- src/app/modules/notes/components/note-list-item/note-list-item.component.ts
+++ src/app/modules/notes/components/note-list-item/note-list-item.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Note} from "../../interfaces/note";
+import {UploadImageService} from "../../../../core/services/upload-image/upload-image.service";
 
 @Component({
   selector: 'app-note-list-item',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./note-list-item.component.scss']
 })
 export class NoteListItemComponent {
+  // Input variables
+  @Input() note: Note | undefined;
 
+  // Component variables
+  colors: { name: string, color: string }[] = [
+    {name: 'Red', color: 'rgb(242, 139, 130)'},
+    {name: 'Orange', color: 'rgb(251, 188, 4)'},
+    {name: 'Yellow', color: 'rgb(255, 244, 117)'},
+    {name: 'Green', color: 'rgb(204, 255, 144)'},
+    {name: 'Blue', color: 'rgb(203, 240, 248)'},
+    {name: 'Dark Blue', color: 'rgb(174, 203, 250)'},
+    {name: 'Purple', color: 'rgb(215, 174, 251)'},
+    {name: 'Pink', color: 'rgb(253, 207, 232)'},
+    {name: 'Brown', color: 'rgb(230, 201, 168)'},
+    {name: 'Gray', color: 'rgb(232, 234, 237)'},
+  ];
+
+  constructor(
+    private uploadImageService: UploadImageService,
+  ) {
+  }
+
+  // Method to remove the image from the note
+  removeNoteImage(): void {
+    this.note!.images = [''];
+  }
+
+  // Method to change the color of a note
+  setNoteColor(color: string | null): void {
+    this.note!.color = color;
+  }
+
+  // Method to upload an image to firebase storage
+  async uploadImage(files: any): Promise<string> {
+    return new Promise<string>(async (resolve, rejects) => {
+      await this.uploadImageService
+        .uploadImage(files.target.files[0])
+        .then((downloadUrl) => {
+          this.note!.images = [downloadUrl];
+          resolve(downloadUrl)
+        })
+        .catch((error) => rejects(error));
+    });
+  }
 }
