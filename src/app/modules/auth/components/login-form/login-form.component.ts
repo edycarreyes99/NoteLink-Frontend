91d9921cfb21a7a333d5f3f036709dad7f69b736
Edycar Reyes <edycarreyes@gmail.com>
2022-11-24 06:52:45 -0600

    configured successfully the login in the project

--- src/app/modules/auth/components/login-form/login-form.component.ts
+++ src/app/modules/auth/components/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import firebase from 'firebase/compat/app'
 
 @Component({
   selector: 'app-login-form',
@@ -7,8 +9,15 @@ import { Component } from '@angular/core';
 })
 export class LoginFormComponent {
 
-  // Method to log in with Google
-  loginWithGoogle() {
+  constructor(
+    private authService: AuthService
+  ) {
+  }
 
+  // Method to log in with Google
+  async loginWithGoogle(): Promise<firebase.User> {
+    return new Promise<firebase.User>(async (resolve, rejects) => {
+      await this.authService.googleLogin().then((res) => resolve(res)).catch((error) => rejects(error));
+    });
   }
 }
