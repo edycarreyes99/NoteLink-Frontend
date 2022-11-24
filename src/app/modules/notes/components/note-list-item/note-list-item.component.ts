b93bf24e76ad5a0237374a6d1b9dbbe0a5cce7bf
Edycar Reyes <edycarreyes@gmail.com>
2022-11-24 02:12:33 -0600

    generated the files for each note item of the list

---
+++ src/app/modules/notes/components/note-list-item/note-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-list-item',
+  templateUrl: './note-list-item.component.html',
+  styleUrls: ['./note-list-item.component.scss']
+})
+export class NoteListItemComponent {
+
+}
